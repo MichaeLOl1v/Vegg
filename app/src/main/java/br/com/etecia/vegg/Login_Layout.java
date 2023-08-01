0f26857176fe8782dd70684a9ea3ceee589939af
MichaelOl1v <micheeloliv@outlook.com>
2023-08-01 19:29:32 -0300

    1

---
+++ app/src/main/java/br/com/etecia/vegg/Login_Layout.java
@@ -0,0 +1,14 @@
+package br.com.etecia.vegg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login_Layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+    }
+}
